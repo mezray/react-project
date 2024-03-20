06810e7da00f896d3a8c6f01ae92ae67a79db5f0
mezray <rmezghani@gmail.com>
2024-03-20 14:18:43 +0100

    Ctrl V failed

--- src/components/TricountForm.tsx
+++ src/components/TricountForm.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState, useEffectgit fetch origin } from "react";
+import React, { useState, useEffect } from "react";
 import { useRouter } from 'next/navigation'
 
 
