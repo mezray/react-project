adf3a9c5a9413c9e2b5e4e54b36680213c49d1df
Rayhan Mezghani <rmezghani@gmail.com>
2024-03-26 16:03:10 +0100

    add logic debt/payer alpha

--- src/app/api/tricount/[id]/route.tsx
+++ src/app/api/tricount/[id]/route.tsx
@@ -4,7 +4,11 @@ export async function GET(request: Request, {params}: {params: {id: string}}) {
   console.log("params",params)
   const id = params.id;
   const tricount = await prisma.cost.findMany({
-    where: { tricountId: Number(id) }
+    where: { tricountId: Number(id) },
+    include: { 
+      payer: true,
+      debtors: true,
+    },
   });
   console.log("tricount",tricount);
   return Response.json(tricount); 
--- src/app/test/[id]/page.tsx
+++ src/app/test/[id]/page.tsx
@@ -1,5 +1,5 @@
-async function getData( id: string  ) {
-  console.log("id",id)
+async function getData(id: string) {
+  console.log("id", id);
   const res = await fetch(`http://localhost:3000/api/tricount/${id}`); // replace '1' with the actual id
   const data = await res.json();
 
@@ -8,7 +8,7 @@ async function getData( id: string  ) {
   };
 }
 
-export default async function Page({params}: {params: {id: string}}) {
+export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
   const {
     props: { data },
@@ -20,17 +20,32 @@ export default async function Page({params}: {params: {id: string}}) {
           <tr>
             <th>Activité</th>
             <th>Prix</th>
-            <th>Tricount ID</th>
-            <th>Payer ID</th>
+            <th>Payer</th>
+            <th>Debtors</th>
           </tr>
         </thead>
         <tbody>
-          {data.map((item, index) => (
+          {data.map((item) => (
             <tr key={item.id}>
               <td>{item.title}</td>
               <td>{item.price}€</td>
-              <td>{item.tricountId}</td>
-              <td>{item.payerId}</td>
+              <td>{item.payer.name}</td>
+              <td>{item.debtors.map((debtor) => debtor.name).join(", ")}</td>
+            </tr>
+          ))}
+          Total: {data.reduce((total, item) => total + item.price, 0)}€
+          {Object.entries(
+            data.reduce((acc, item) => {
+              item.debtors.forEach((debtor) => {
+                acc[debtor.name] =
+                  (acc[debtor.name] || 0) + item.price / item.debtors.length;
+              });
+              return acc;
+            }, {})
+          ).map(([name, debt], index) => (
+            <tr key={index}>
+              <td>{name} debt: </td>
+              <td>{debt.toFixed(2)}€</td>
             </tr>
           ))}
         </tbody>
